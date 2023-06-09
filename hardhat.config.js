243288157d89be7580929c860138353bdda4d132
Julian Cabrera <a.j.c.marce@gmail.com>
2023-06-09 05:56:42 -0300

    sepolia migration succeed

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,9 +7,9 @@ require("hardhat-contract-sizer")
 require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
-const GOERLI_CHAIN_ID = process.env.GOERLI_CHAIN_ID
-const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const SEPOLIA_CHAIN_ID = process.env.SEPOLIA_CHAIN_ID
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 module.exports = {
@@ -27,10 +27,10 @@ module.exports = {
             chainId: 31337,
             blockConfirmations: 1,
         },
-        goerli: {
-            url: GOERLI_RPC_URL,
-            accounts: [GOERLI_PRIVATE_KEY],
-            chainId: Number(GOERLI_CHAIN_ID),
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [SEPOLIA_PRIVATE_KEY],
+            chainId: Number(SEPOLIA_CHAIN_ID),
             blockConfirmations: 6,
         },
     },
@@ -55,7 +55,7 @@ module.exports = {
     },
     etherscan: {
         apiKey: {
-            goerli: ETHERSCAN_API_KEY,
+            sepolia: ETHERSCAN_API_KEY,
         },
     },
 }
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -8,12 +8,12 @@ const networkConfig = {
         callbackGasLimit: "5000000",
         interval: "30",
     },
-    5: {
-        name: "goerli",
-        vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+    11155111: {
+        name: "sepolia",
+        vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
         entranceFee: ethers.utils.parseEther("0.01"),
-        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
-        subscriptionId: "12036",
+        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
+        subscriptionId: "2690",
         callbackGasLimit: "5000000",
         interval: "30",
     },
